7cc754ce1ce6b0f2cbfc8a0b3b1096257fedf261
CEEJ987 <123979158+CEEJ987@users.noreply.github.com>
2023-04-24 20:18:59 -0500

    Update Assignment

---
+++ App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./Header"
+import Footer from "./Footer"
+import Main from "./Main"
+import SelectedBeast from './Selectedbeast';
+import HornedBeast from './HornedBeast';
+import Container from 'react-bootstrap/Container';
+import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react';
+
+function App() {
+  const [displayselectedBeast, setdisplayselectedBeast] = useState(false)
+  const [SelectedBeast, setSelectedBeast] = useState(undefined)//{title: 'test', imgurl: "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg", description: 'test'})
+
+  // let selectedBeastHtml = <SelectedBeast currentBeast={SelectedBeast} updateFunction={setdisplayselectedBeast}/>
+  // if(displayselectedBeast === false){
+  //   selectedBeastHtml = <></>
+  // }
+
+  return (
+    <div className="App">
+      <Container>
+
+      
+      <Header/>
+      {displayselectedBeast && <SelectedBeast currentBeast={SelectedBeast} updateFunction={setdisplayselectedBeast}/>}
+      <Main  setSelectedBeast={setSelectedBeast} updateFunction={setdisplayselectedBeast}/>
+      
+ 
+      <Footer/>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
