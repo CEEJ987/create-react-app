6b0689620828b740f4dcd50fe1ad7259f9e0ff38
CEEJ987 <123979158+CEEJ987@users.noreply.github.com>
2023-05-02 12:24:49 -0500

    intialized origin sort

--- src/Main.js
+++ src/Main.js
@@ -4,25 +4,41 @@ import data from "./data.json"
 import HornedBeast from './HornedBeast';
 import { Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
 function Main(props) {
+  //let choice = "One"
+  const [choice, setChoice] = useState(1)
   var beasts = [<HornedBeast />, <HornedBeast />]
-  var moreBeasts = data.map(function (element) {
-    return <HornedBeast imgurl={element.image_url} description={element.description} title={element.title} />
+  var filterdData = data.filter(function(element){
+    console.log(element)
+    if (element.horns == choice){
+      return true
+    }
+    else {
+      return false
+    }
   })
   return (
 
     <div>
-      <Form.Select aria-label="Number of Horns Selection">
+      <Form.Select onChange={function(event){
+        //choice = event.target.value 
+        setChoice(event.target.value)
+
+
+      }} aria-label="Number of Horns Selection">
         <option>Number of Horns Selection</option>
         <option value="1">One</option>
         <option value="2">Two</option>
-        <option value="3">Three</option>
+        <option value="100">Hundred</option>
       </Form.Select>
-      { data.map((element) => {
+      { filterdData.map((element) => {
           return <HornedBeast  setSelectedBeast={props.setSelectedBeast} updateFunction={props.updateFunction} imgurl={element.image_url} description={element.description} title={element.title} />
           })} 
+          
       <h1>{props.title}</h1>
+    <h1>{choice}</h1>
       
     </div>
 
